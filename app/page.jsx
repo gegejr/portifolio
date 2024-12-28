416c738c3df8500cfe2736ce0d68b0abadbdcdb3
bruno.lima <brunopinheirolima9@gmail.com>
2024-12-28 16:52:17 -0400

    landingpage

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto">homepage</div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ app/services/page.jsx
@@ -0,0 +1,5 @@
+const Services = () => {
+    return <div>servies page</div>;
+};
+
+export default Services;
