3233b6f500da114ba385348fd13eb4c2ba0c4aec
gegejr <geo.xodrom@gmail.com>
2025-01-10 08:59:14 -0400

    finalizado work e contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -49,25 +49,61 @@ const Contact = () => {
             <div className="container mx-auto"> {/* distancia dos itens da borda*/}
                 <div className="flex flex-col xl:flex-row gap-[30px]"> {/* distancia dos itens na vertical*/}
                     {/*form */}
-                    <div className="xl:h-[54%] order-2 xl:order-none"> {/* No modo de tela smartphone, o form vai para baixo e info fica acima*/}
+                    <div className="xl:w-[54%] order-2 xl:order-none"> {/* No modo de tela smartphone, o form vai para baixo e info fica acima*/}
                         <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"> {/* flex flex-col gap-6 = estilização do item ; p-10 = ordena o item dentro do campo ; bg-[#27272c] = define cor do campo */}
                             <h3 className="text-4xl text-accent"> {/* text-4xl = defini tamanho da fonte ; text-accent = cor da fonte*/} Let's work together</h3>
                             <p className="text-white/60">
                             Lorem ipsum dolor sit amet consectetur 
                             adipisicing elit. Expedita, iusto, in fuga sunt, quae voluptate.
                             </p>
+                            {/* input */}
                             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                                 <Input type="firstname" placeholder="Fiirstname"/>  {/*Adiciona campo de texto input*/}
                                 <Input type="lastname" placeholder="Lastname"/>
                                 <Input type="email" placeholder="Email address"/>
                                 <Input type="phone" placeholder="Phone"/>
                             </div>
+                            {/* select */}
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select a service" />  
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Select a service</SelectLabel>
+                                        <SelectItem value="est">Web Development</SelectItem>
+                                        <SelectItem value="cst">UI/UX Design</SelectItem>
+                                        <SelectItem value="mst">Logo Design</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                            {/* textarea */}
+                            <Textarea
+                                className="h-[200px]"
+                                placeholder="Type your mensage here."
+                            />
+                            {/* btn */}
+                            <Button size="md" className="max-w-40">
+                                Send message
+                            </Button>
                         </form>
                     </div>
                     {/*info */}
                     <div className="flex-1 flex items-center xl:justify-end order-1
                     xl:order-none mb-8 xl:mb-0"> {/* Aumenta a distancia do item*/}
-                        info
+                        <ul className="flex flex-col gap-10">
+                            {info.map((item, index) => {
+                                return <li key={index} className="flex items-center gap-6">
+                                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                                        <div className="text-[28px]">{item.icon}</div>
+                                    </div>
+                                    <div className="flex-1">
+                                        <p className="text-white/60">{item.title}</p>
+                                        <h3 className="text-xl">{item.description}</h3>
+                                    </div>
+                                </li>
+                            })}
+                        </ul>
                     </div>
                 </div>
             </div>
